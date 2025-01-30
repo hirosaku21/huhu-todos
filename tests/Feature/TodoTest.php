add3646445ba9275b416a798376fd9de5c12c259
hirosaku21 <hiroyuki_scorpio3@hotmail.co.jp>
2025-01-30 09:47:27 +0900

    Featureテストサンプル追加

---
+++ tests/Feature/TodoTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
+
+class TodoTest2 extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh --seed');
+    }
+
+    public function test_ログイン画面以外にアクセスするとログイン画面にリダイレクトされる(): void
+    {
+        $response = $this->get('/admin');
+        $response->assertRedirect('/login');
+    }
+
+    public function test_ログイン認証してTOP画面表示(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_ユーザーが登録されてるか(): void
+    {
+        $response = $this->assertDatabaseCount('users', 2);
+    }
+}
