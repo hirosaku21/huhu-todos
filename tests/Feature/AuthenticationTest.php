989dd387838f31bc0317702cf3a1e660bf6e540d
hirosaku21 <hiroyuki_scorpio3@hotmail.co.jp>
2025-01-30 18:16:58 +0900

    fix

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -3,12 +3,20 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Artisan;
+
 
 class AuthenticationTest extends TestCase
 {
-    // use RefreshDatabase;
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh --seed');
+    }
 
     public function test_login_screen_can_be_rendered(): void
     {
