147bafb9e2a4904326891d51d97187a0030586ce
hirosaku21 <hiroyuki_scorpio3@hotmail.co.jp>
2025-01-30 09:52:39 +0900

    FeatureTest修正

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_login_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
